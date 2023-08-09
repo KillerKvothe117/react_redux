1aed663deddfe53825faca5edd634b5b8da7f973
Kosi <iyiegbuk@gmail.com>
2023-08-09 13:04:20 +0100

    done with counter, setup news feed next

---
+++ Lesson_1/src/App.jsx
@@ -0,0 +1,12 @@
+import Counter from "./features/counter/Counter";
+import "./index.css"
+
+function App() {
+  return (
+    <main className="App">
+      <Counter />
+    </main>
+  );
+}
+
+export default App;
