70e96bb29ad90d84ffac0d38b348349ff609ef40
Kvothe <iyiegbuk@gmail.com>
2023-10-02 10:24:56 +0100

    added async thunks

--- Lesson_2/src/features/posts/AddPostForm.jsx
+++ Lesson_2/src/features/posts/AddPostForm.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { postAdded } from "./postsSlice";
+import { addNewPost } from "./postsSlice";
 import { selectAllUsers } from "../users/usersSlice";
 
 const AddPostForm = () => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [userId, setUSerId] = useState("");
+  const [addRequestStatus, setAddRequestStatus] = useState("idle");
 
   const users = useSelector(selectAllUsers);
 
@@ -16,16 +17,25 @@ const AddPostForm = () => {
 
   const dispatch = useDispatch();
 
-  const onSavePostClicked = () => {
-    if (title && content) {
-      dispatch(postAdded(title, content, userId));
+  const canSave =
+    [title, content, userId].every(Boolean) && addRequestStatus === "idle";
 
-      setTitle("");
-      setContent("");
+  const onSavePostClicked = () => {
+    if (canSave) {
+      try {
+        setAddRequestStatus("pending");
+        dispatch(addNewPost({ title, body: content, userId })).unwrap();
+
+        setTitle("");
+        setContent("");
+        setUSerId("");
+      } catch (err) {
+        console.error("Failed to save the post", err);
+      } finally {
+        setAddRequestStatus("idle");
+      }
     }
   };
-
-  const canSave = Boolean(title) && Boolean(content) && Boolean(userId);
 
   const userOptions = users.map((user) => (
     <option key={user.id} value={user.id}>
@@ -60,7 +70,7 @@ const AddPostForm = () => {
           onChange={onContentChanged}
         />
 
-        <button type="button" onClick={onSavePostClicked} disabled={!canSave }>
+        <button type="button" onClick={onSavePostClicked} disabled={!canSave}>
           Save Post
         </button>
       </form>
--- Lesson_2/src/index.jsx
+++ Lesson_2/src/index.jsx
@@ -4,11 +4,12 @@ import App from "./App.jsx";
 import "./index.css";
 import { store } from "./store.jsx";
 import { Provider } from "react-redux";
+import { fetchUsers } from "./features/users/usersSlice.jsx";
+
+store.dispatch(fetchUsers());
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
 );
